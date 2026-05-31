20621e2b9c2633f7be3b245da4123649795df365
Pablo Camardelli <pecamardelli@gmail.com>
2026-05-30 22:24:38 -0300

    test(combat): cover Proximity argument-order symmetry

--- test/unit/test_proximity.gd
+++ test/unit/test_proximity.gd
@@ -12,6 +12,11 @@ func test_not_within_when_z_exceeds():
 func test_boundary_is_inclusive():
 	assert_true(Proximity.is_within(Vector2(0, 0), Vector2(50, 45), 50, 45))
 
+func test_symmetric_in_argument_order():
+	# |Δ| means b-left-of-a is the same as b-right-of-a (guards a future sign flip).
+	assert_true(Proximity.is_within(Vector2(150, 400), Vector2(110, 370), 50, 45))
+	assert_false(Proximity.is_within(Vector2(150, 400), Vector2(90, 400), 50, 45))
+
 func test_thresholds_are_the_arcade_values():
 	assert_eq(Proximity.CLOSE_DX, 50.0)
 	assert_eq(Proximity.CLOSE_DZ, 45.0)
