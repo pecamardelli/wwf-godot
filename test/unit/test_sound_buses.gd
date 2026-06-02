e3185dc6fc63134b2847fac75ecd0a8164c7fba6
Pablo Camardelli <pecamardelli@gmail.com>
2026-06-01 22:47:11 -0300

    test(audio): assert messages + Music routing in bus test

--- test/unit/test_sound_buses.gd
+++ test/unit/test_sound_buses.gd
@@ -7,6 +7,7 @@ func test_buses_exist():
 	assert_true(AudioServer.get_bus_index("Voice") >= 0, "Voice bus present")
 	assert_true(AudioServer.get_bus_index("Music") >= 0, "Music bus present (stub)")
 
-func test_sfx_and_voice_route_to_master():
-	assert_eq(AudioServer.get_bus_send(AudioServer.get_bus_index("SFX")), &"Master")
-	assert_eq(AudioServer.get_bus_send(AudioServer.get_bus_index("Voice")), &"Master")
+func test_child_buses_route_to_master():
+	assert_eq(AudioServer.get_bus_send(AudioServer.get_bus_index("SFX")), &"Master", "SFX routes to Master")
+	assert_eq(AudioServer.get_bus_send(AudioServer.get_bus_index("Voice")), &"Master", "Voice routes to Master")
+	assert_eq(AudioServer.get_bus_send(AudioServer.get_bus_index("Music")), &"Master", "Music routes to Master")
